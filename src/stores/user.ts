4221829a2ac5bcfc68a81c11f86d302012c30586
iak1p <iak1p@eduroam-160-102.fiit.stuba.sk>
2025-09-30 13:11:35 +0200

    ws

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,7 +3,8 @@ import { defineStore } from "pinia";
 export const useChatsStore = defineStore("user", {
   state: () => ({
     username: null as string | null,
-    status: null as string | null
+    status: null as string | null,
+    id: null as number | null
   }),
   actions: {
     
